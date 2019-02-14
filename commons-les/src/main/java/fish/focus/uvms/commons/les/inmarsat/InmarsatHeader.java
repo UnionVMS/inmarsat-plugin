31d12518cb2a6bcbdecfc86fe90d1d4ad5097a6d
HAV\thofan <thomas.fanto@havochvatten.se>
2019-02-14 13:36:36 +0100

    remove eoh check

--- commons-les/src/main/java/fish/focus/uvms/commons/les/inmarsat/InmarsatHeader.java
+++ commons-les/src/main/java/fish/focus/uvms/commons/les/inmarsat/InmarsatHeader.java
@@ -140,8 +140,8 @@ public class InmarsatHeader {
 		return headerToValidate != null && API_SOH == headerToValidate[HeaderStruct.POS_START_OF_HEADER_POS]
 				&& API_LEAD_TEXT.getBytes()[0] == headerToValidate[HeaderStruct.POS_LEAD_TEXT_0]
 				&& API_LEAD_TEXT.getBytes()[1] == headerToValidate[HeaderStruct.POS_LEAD_TEXT_1]
-				&& API_LEAD_TEXT.getBytes()[2] == headerToValidate[HeaderStruct.POS_LEAD_TEXT_2]
-				&& API_EOH == headerToValidate[headerToValidate.length - 1];
+				&& API_LEAD_TEXT.getBytes()[2] == headerToValidate[HeaderStruct.POS_LEAD_TEXT_2];
+				// && API_EOH == headerToValidate[headerToValidate.length - 1];
 
 	}
 
