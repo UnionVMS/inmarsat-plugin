2b5205122261e91ddc150f8c455b4fa24a5d65d0
thomasfanto <thomas.fanto@havochvatten.se>
2019-05-08 16:34:36 +0200

    onMessage work

--- service/src/main/java/eu/europa/ec/fisheries/uvms/plugins/inmarsat/InmarsatMessageListener.java
+++ service/src/main/java/eu/europa/ec/fisheries/uvms/plugins/inmarsat/InmarsatMessageListener.java
@@ -6,6 +6,8 @@ import org.slf4j.LoggerFactory;
 
 import javax.ejb.ActivationConfigProperty;
 import javax.ejb.MessageDriven;
+import javax.jms.BytesMessage;
+import javax.jms.JMSException;
 import javax.jms.Message;
 import javax.jms.MessageListener;
 
@@ -20,15 +22,45 @@ import javax.jms.MessageListener;
 
 public class InmarsatMessageListener implements MessageListener {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(InmarsatMessageListener.class);
-
+    private static final Logger LOG = LoggerFactory.getLogger(InmarsatMessageListener.class);
 
 
     @Override
     public void onMessage(Message message) {
 
-        LOGGER.info("GOT A MESSAGE HURRA !!!!!!!!!!!!!!!!!!!!!!");
+        byte[] payload = null;
+
+        try {
+            if (message instanceof BytesMessage) {
+                payload = message.getBody(byte[].class);
+                if(payload != null){
+                    String msg = bytesArrayToHexString(payload);
+                    LOG.info(msg);
+
+                    // interpret and post to exchange . . .
 
 
+                }
+            }
+        } catch (JMSException e) {
+            LOG.error(e.toString(), e);
+        }
     }
+
+
+    public String bytesArrayToHexString(byte[] bytes) {
+
+        char[] hexArray = "0123456789ABCDEF".toCharArray();
+
+        char[] hexChars = new char[bytes.length * 2];
+        for (int j = 0; j < bytes.length; j++) {
+            int v = bytes[j] & 0xFF;
+            hexChars[j * 2] = hexArray[v >>> 4];
+            hexChars[j * 2 + 1] = hexArray[v & 0x0F];
+        }
+        return new String(hexChars);
+    }
+
+
+
 }
